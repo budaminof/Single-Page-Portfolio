9a060fd2e87ed320595f145c556535e0324ad73a
Bud Aminof <budaminof@gmail.com>
2018-03-28 20:41:13 -0600

    fixed Typist key errors

--- src/App.js
+++ src/App.js
@@ -13,63 +13,62 @@ class App extends Component {
 
         <div>
           <Typist
+            key="first"
             cursor={{ hideWhenDone: true, show: false }}
             avgTypingDelay={20}
             className="header" >
             BUD AMINOF
             <img src={twitter} alt="twitter-icon"></img>
           </Typist>
+          <Typist
+            key="second"
+            cursor={{
+              hideWhenDone: true,
+              blink: true,
+              }}
+            startDelay={200}
+            avgTypingDelay={25}
+          >
+            · I love all things JS: React, Redux, Node.js and Express.js;
+          <Typist.Delay ms={200} /> 
+            <br />
+            <br />
+            · On my free time I build Ethereum applications and talk blockchain;
+          <Typist.Delay ms={200} />
+            <br />
+            <br />
+            · I can jump right into an Angu<Typist.Delay ms={200} />alr
+          <Typist.Backspace count={3} delay={300} />
+          <Typist.Delay ms={300} />
+            lar project and work on Postgres DB;
+          <Typist.Delay ms={200} />
+            <br />
+            <br />
+            · Talk to me about code, music and surf.
+            <br />
 
-          <div>
-             <Typist
-                cursor={{
-                  hideWhenDone: true,
-                  blink: true,
-                 }}
-                startDelay={200}
-                avgTypingDelay={25}
-              >
-                · I love all things JS: React, Redux, Node.js and Express.js;
-              <Typist.Delay ms={200} /> 
-                <br />
-                <br />
-                · On my free time I build Ethereum applications and talk blockchain;
+            <div className="image-wrapper">
               <Typist.Delay ms={200} />
-                <br />
-                <br />
-                · I can jump right into an Angu<Typist.Delay ms={200} />alr
-              <Typist.Backspace count={3} delay={300} />
+              <a href="mailto:budaminof@gmail.com">
+                <img src={email} alt="email-icon"></img></a>
+
               <Typist.Delay ms={300} />
-                lar project and work on Postgres DB;
+              <a target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/budaminof/">
+                <img src={linkedin} alt="linkedin-icon"></img></a>
+              
               <Typist.Delay ms={200} />
-                <br />
-                <br />
-                · Talk to me about code, music and surf.
-                <br />
+              <a target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/bud_aminof">
+                <img src={twitter} alt="twitter-icon"></img></a>
 
-                <div className="image-wrapper">
-                  <Typist.Delay ms={200} />
-                  <a href="mailto:budaminof@gmail.com">
-                    <img src={email} alt="email-icon"></img></a>
+              
+              <Typist.Delay ms={100} />
+            </div>
 
-                  <Typist.Delay ms={300} />
-                  <a target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.linkedin.com/in/budaminof/">
-                    <img src={linkedin} alt="linkedin-icon"></img></a>
-                 
-                  <Typist.Delay ms={200} />
-                  <a target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://twitter.com/bud_aminof">
-                    <img src={twitter} alt="twitter-icon"></img></a>
-
-                  
-                  <Typist.Delay ms={100} />
-                </div>
-
-              </Typist>
-          </div>
+          </Typist>
 
         </div>
       </div>
