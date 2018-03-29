d7dfe91529aea120bd88f8ad4439e3700b2946e9
Bud Aminof <budaminof@gmail.com>
2018-03-29 11:42:58 -0600

    added logs, and unique keys

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ import './App.css';
 class App extends Component {
 
   render() {
+
+    console.log("=====================================");
+    console.log("I am so happy to see you here!")
+    console.log("Lets play with Ethereum contracts. Do you have MetaMask installed?");
+    console.log("https://metamask.io/");
+    console.log("My contract is currently a work in progress but I will deploy it very soon.");
+    console.log("=====================================");
+
     return (
       <div className="App">
 
         <div>
           <Typist
-            key="header"
+            key={1}
             cursor={{ 
               hideWhenDone: true, 
               show: false }}
@@ -23,7 +31,7 @@ class App extends Component {
             <img src={twitter} alt="twitter-icon"></img>
           </Typist>
           <Typist
-            key="body"
+            key={2}
             cursor={{
               hideWhenDone: true,
               blink: true,
@@ -33,21 +41,21 @@ class App extends Component {
           >
             · I love all things JS: React, Redux, Node.js and Express.js;
           <Typist.Delay ms={200} /> 
-            <br key="1"/>
-            <br key="2" className="mobile"/>
+            <br key={3}/>
+            <br key={4} className="mobile"/>
             · On my free time I build Ethereum applications and talk blockchain;
           <Typist.Delay ms={200} />
-            <br key="3"/>
-            <br key="4" className="mobile"/>
+            <br key={5}/>
+            <br key={6} className="mobile"/>
             · I can jump right into an Angu<Typist.Delay ms={200} />alr
           <Typist.Backspace count={3} delay={300} />
           <Typist.Delay ms={300} />
             lar project and work on Postgres DB;
           <Typist.Delay ms={200} />
-            <br key="5"/>
-            <br key="6"className="mobile"/>
+            <br key={7}/>
+            <br key={8} className="mobile"/>
             · Talk to me about code, music and surf.
-            <br key="7" className="mobile"/>
+            <br key={9} className="mobile"/>
 
             <div className="image-wrapper">
               <Typist.Delay ms={200} />
@@ -65,9 +73,7 @@ class App extends Component {
                 rel="noopener noreferrer"
                 href="https://twitter.com/bud_aminof">
                 <img src={twitter} alt="twitter-icon"></img></a>
-
-              
-              <Typist.Delay ms={100} />
+                <Typist.Delay ms={100} />
             </div>
 
           </Typist>
